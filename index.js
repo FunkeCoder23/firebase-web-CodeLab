4c781b1cdd68d1b9c336a586daa2bc33dfdd6812
FunkeCoder23 <timelessrock3r23@gmail.com>
2020-11-11 11:13:23 -0500

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
 
@@ -7,28 +7,36 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-import * as firebaseui from 'firebaseui';
+import * as firebaseui from "firebaseui";
 
 // Document elements
-const startRsvpButton = document.getElementById('startRsvp');
-const guestbookContainer = document.getElementById('guestbook-container');
+const startRsvpButton = document.getElementById("startRsvp");
+const guestbookContainer = document.getElementById("guestbook-container");
 
-const form = document.getElementById('leave-message');
-const input = document.getElementById('message');
-const guestbook = document.getElementById('guestbook');
-const numberAttending = document.getElementById('number-attending');
-const rsvpYes = document.getElementById('rsvp-yes');
-const rsvpNo = document.getElementById('rsvp-no');
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const numberAttending = document.getElementById("number-attending");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
 
 var rsvpListener = null;
 var guestbookListener = null;
 
 async function main() {
+  <script src="https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js" />;
 
   // Add Firebase project configuration object here
-  // var firebaseConfig = {};
-
-  // firebase.initializeApp(firebaseConfig);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNtOf36nW9Q-u0f-UWXzJV0zgrGja_Xvo",
+    authDomain: "sample-faafb.firebaseapp.com",
+    databaseURL: "https://sample-faafb.firebaseio.com",
+    projectId: "sample-faafb",
+    storageBucket: "sample-faafb.appspot.com",
+    messagingSenderId: "433378267664",
+    appId: "1:433378267664:web:a9ab7ead17145eab543095"
+  };
+  firebase.initializeApp(firebaseConfig);
 
   // FirebaseUI config
   const uiConfig = {
@@ -46,7 +54,10 @@ async function main() {
     }
   };
 
-  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // Listen to RSVP button clicks
+  startRsvpButton.addEventListener("click", () => {
+    ui.start("#firebaseui-auth-container", uiConfig);
+  });
 }
 main();
-
